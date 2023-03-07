ed13e8feebc19736f954cade33fa86ea08c63c0c
chleb <bszlacht@student.agh.edu.pl>
2023-03-07 01:45:11 +0100

    dasdasdassda

--- BufferedSend.py
+++ BufferedSend.py
@@ -64,28 +64,28 @@ def test(p_rank):
 
 test(rank)
 
-# Define the buffer size
-buf_size = 1024
-
-# Allocate memory for the buffer
-buffer = np.empty(buf_size, dtype='b')
-
-# Attach the buffer to the process
-MPI.Attach_buffer(buffer)
-
-if rank == 0:
-    # Receive the message sent from rank 1
-    msg = comm.Irecv(buffer, source=1, tag=123)
-    msg.Wait()
-    print("Rank 0 received message:", buffer.tobytes())
-else:
-    # Prepare a message to be sent
-    msg = b"Hello from rank " + str(rank).encode()
-    # Send the message using MPI.Bsend()
-    comm.Bsend([msg, MPI.CHAR], dest=0, tag=123)
-
-# Detach the buffer from the process
-MPI.Detach_buffer()
-
-# Free the memory allocated for the buffer
-del buffer
+# # Define the buffer size
+# buf_size = 1024
+#
+# # Allocate memory for the buffer
+# buffer = np.empty(buf_size, dtype='b')
+#
+# # Attach the buffer to the process
+# MPI.Attach_buffer(buffer)
+#
+# if rank == 0:
+#     # Receive the message sent from rank 1
+#     msg = comm.Irecv(buffer, source=1, tag=123)
+#     msg.Wait()
+#     print("Rank 0 received message:", buffer.tobytes())
+# else:
+#     # Prepare a message to be sent
+#     msg = b"Hello from rank " + str(rank).encode()
+#     # Send the message using MPI.Bsend()
+#     comm.Bsend([msg, MPI.CHAR], dest=0, tag=123)
+#
+# # Detach the buffer from the process
+# MPI.Detach_buffer()
+#
+# # Free the memory allocated for the buffer
+# del buffer
