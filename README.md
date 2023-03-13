c84cacd15af4b09ff836b4bdb5f2695df7df058c
chleb <bszlacht@student.agh.edu.pl>
2023-03-14 00:11:59 +0100

    needs refactor

--- README.md
+++ README.md
@@ -5,4 +5,7 @@
 ![alt_text](https://github.com/bszlacht/mpr/blob/main/plots/bs21.png)
 ![alt_text](https://github.com/bszlacht/mpr/blob/main/plots/ss12.png)
 ![alt_text](https://github.com/bszlacht/mpr/blob/main/plots/ss21.png)
+![alt_text](https://github.com/bszlacht/mpr/blob/main/plots/comparisononenode.png)
+![alt_text](https://github.com/bszlacht/mpr/blob/main/plots/twonodescomparison.png)
+
 
--- ploter.py
+++ ploter.py
@@ -4,10 +4,12 @@ from matplotlib import pyplot as plt
 columns = ["bandwidth", "bsize"]
 
 # df = pd.read_csv("std_one_node", usecols=columns)
-# plt.plot(df.bsize, df.bandwidth)
+# df1 = pd.read_csv("buf_one_node", usecols=columns)
+# plt.plot(df1.bsize, df1.bandwidth, color='r', label='buf_one_node')
+# plt.plot(df.bsize, df.bandwidth, color='b', label='std_one_node')
 # plt.xlabel('siz e in B')
 # plt.ylabel('bandwidth in MB/s')
-# plt.title('Standard Send 1:2')
+# plt.title('One node methods comparison')
 
 
 # df = pd.read_csv("std_two_nodes", usecols=columns)
@@ -16,18 +18,19 @@ columns = ["bandwidth", "bsize"]
 # plt.ylabel('bandwidth in MB/s')
 # plt.title('Standard Send 2:1')
 
-
-# df = pd.read_csv("buf_one_node", usecols=columns)
+# df = pd.read_csv("buf_two_nodes", usecols=columns)
 # plt.plot(df.bsize, df.bandwidth)
 # plt.xlabel('size in B')
 # plt.ylabel('bandwidth in MB/s')
-# plt.title('Buffered Send 1:2')
+# plt.title('Buffered Send 2:1')
 
-df = pd.read_csv("buf_two_nodes", usecols=columns)
-plt.plot(df.bsize, df.bandwidth)
-plt.xlabel('size in B')
+df = pd.read_csv("std_two_nodes", usecols=columns)
+df1 = pd.read_csv("buf_two_nodes", usecols=columns)
+plt.plot(df1.bsize, df1.bandwidth, color='r', label='buf_two_nodes')
+plt.plot(df.bsize, df.bandwidth, color='b', label='std_one_nodes')
+plt.xlabel('siz e in B')
 plt.ylabel('bandwidth in MB/s')
-plt.title('Buffered Send 2:1')
+plt.title('Two nodes methods comparison')
 
 plt.show()
 
