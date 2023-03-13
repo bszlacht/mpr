2ca97a1e3b3240c673fbfc8f7b8103154f25a3f4
chleb <bszlacht@student.agh.edu.pl>
2023-03-13 21:22:51 +0100

    needs refactor

--- ploter.py
+++ ploter.py
@@ -3,11 +3,11 @@ from matplotlib import pyplot as plt
 
 columns = ["bandwidth", "bsize"]
 
-df = pd.read_csv("std_one_node", usecols=columns)
-plt.plot(df.bsize, df.bandwidth)
-plt.xlabel('siz e in B')
-plt.ylabel('bandwidth in MB/s')
-plt.title('Standard Send 1:2')
+# df = pd.read_csv("std_one_node", usecols=columns)
+# plt.plot(df.bsize, df.bandwidth)
+# plt.xlabel('siz e in B')
+# plt.ylabel('bandwidth in MB/s')
+# plt.title('Standard Send 1:2')
 
 
 # df = pd.read_csv("std_two_nodes", usecols=columns)
@@ -23,11 +23,11 @@ plt.title('Standard Send 1:2')
 # plt.ylabel('bandwidth in MB/s')
 # plt.title('Buffered Send 1:2')
 
-# df = pd.read_csv("buf_two_nodes", usecols=columns)
-# plt.plot(df.bsize, df.bandwidth)
-# plt.xlabel('size in B')
-# plt.ylabel('bandwidth in MB/s')
-# plt.title('Buffered Send 2:1')
+df = pd.read_csv("buf_two_nodes", usecols=columns)
+plt.plot(df.bsize, df.bandwidth)
+plt.xlabel('size in B')
+plt.ylabel('bandwidth in MB/s')
+plt.title('Buffered Send 2:1')
 
 plt.show()
 
