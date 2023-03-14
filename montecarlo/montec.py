8401fb26a5b31ea7ad00a4bdfafdf1f3a8e0131e
chleb <bszlacht@student.agh.edu.pl>
2023-03-14 13:53:39 +0100

    needs refactor

---
+++ montecarlo/montec.py
@@ -0,0 +1,46 @@
+import random
+import math
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+n = 10000000
+
+# todo pokazac dokladnosc
+# todo pokazac ze ze wzrostem ilosci punktow wzrasta dokladnosc pi
+
+'''
+T => szybkość algorytmu
+Sp = T1/Tn => speed up
+E = Sp/n => wydajność czyli jak dobrze nam poszlo w porownaniu do oryginalnego speed upu?? (bardzo blisko 1) (jak jest powyzej 1 to niepokoj)
+Sf = (1/Sp - 1/n) / (1 - 1/n) => metryka karpafrata?
+Wykresy mają być nieciągłe bo nie ma np. pół procesora WYRAZNE PUNKTY + PRZEWYRANA LINIA!!!
+
+'''
+
+
+def generate_points(n_local):
+    inCount = 0
+    random.seed(rank)
+    for i in range(n_local):
+        x, y = random.random(), random.random()
+        d = math.sqrt(pow(x, 2) + pow(y, 2))
+        if d < 1:
+            inCount += 1
+    return inCount
+
+
+def monte_carlo():
+    inCount = generate_points(n // size)
+    print(inCount)
+    inCountAll = comm.reduce(inCount, op=MPI.SUM, root=0)
+    comm.Barrier()
+    if rank == 0:
+        pi_estimate = 4 * inCountAll / n
+        print("Estimated value of pi:", pi_estimate)
+
+
+start_time = MPI.Wtime()
+monte_carlo()
+end_time = (MPI.Wtime() - start_time)
