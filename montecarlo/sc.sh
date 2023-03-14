8d15294df7f9dba39620058a39bbc86075bd5576
chleb <bszlacht@student.agh.edu.pl>
2023-03-14 14:04:54 +0100

    needs refactor

--- montecarlo/sc.sh
+++ montecarlo/sc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
-#SBATCH --ntasks 12
-#SBATCH --time=01:00:00
+#SBATCH --ntasks 6
+#SBATCH --time=00:03:00
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr23-cpu
