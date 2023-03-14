8d567845faafb91aef2385801276d084c28b4344
chleb <bszlacht@student.agh.edu.pl>
2023-03-14 14:12:10 +0100

    needs refactor

--- montecarlo/sc.sh
+++ montecarlo/sc.sh
@@ -4,3 +4,6 @@
 #SBATCH --time=00:03:00
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr23-cpu
+module add scipy-bundle/2021.10-foss-2021b
+chmod +x montec.py
+mpiexec -machinefile ./allnodes -np 6 ./montec.py
