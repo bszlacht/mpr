f231a8352beba47ab0e16ebabbad6e94c3eeff73
chleb <bszlacht@student.agh.edu.pl>
2023-03-20 16:56:49 +0100

    needs refactor

--- montecarlo/vc.sh
+++ montecarlo/vc.sh
@@ -2,5 +2,5 @@
 chmod +x montec.py
 for THREADS in 12 11 10 9 8 7 6 5 4 3 2 1
 do
-  mpiexec -machinefile ./allnodes -np $THREADS ./montec.py 1000000
+  mpiexec -machinefile ./allnodes -np $THREADS ./montec.py 5000000
 done
