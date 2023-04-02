3df139b326adcfcf69f1b23db197274b126502eb
bsz <bszlacht@student.agh.edu.pl>
2023-04-02 16:45:10 +0200

    asdd

--- zad2/sc.sh
+++ zad2/sc.sh
@@ -1,8 +1,9 @@
+# max = 32761316000B
 # shellcheck disable=SC2034
-for ARR_SIZE in 100 1000 10000 100000 1000000 10000000 100000000
+for ARR_SIZE in 100 1000 10000 100000 1000000 10000000 100000000 1000000000
 do
     # shellcheck disable=SC2043
-    for THREADS in 1 2 3 4 5 6 7 8 9 10 11 12
+    for THREADS in 1 2 3 4
     do
       ./main $ARR_SIZE $THREADS
     done
