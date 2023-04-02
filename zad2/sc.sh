15fb4a55ba3963e01237664a960621de2ff50aa0
bsz <bszlacht@student.agh.edu.pl>
2023-04-02 15:08:28 +0200

    asd

--- zad2/sc.sh
+++ zad2/sc.sh
@@ -1,9 +1,9 @@
 # shellcheck disable=SC2034
-for ARR_SIZE in 4095164500 2047582250 1023791125 5511895562
+for ARR_SIZE in 10000 1000 100
 do
     # shellcheck disable=SC2043
     # 1 2 3 4 5 6 7 8 9 10 11
-    for THREADS in 12
+    for THREADS in 10 11 12
     do
       ./main ARR_SIZE THREADS
     done
