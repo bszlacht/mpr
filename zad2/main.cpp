ea1a6ba0b759ca637a1cea2655a6f946cbbe9211
bsz <bszlacht@student.agh.edu.pl>
2023-04-02 20:23:53 +0200

    asdad

--- zad2/main.cpp
+++ zad2/main.cpp
@@ -46,6 +46,6 @@ int main(int argc, char **argv)
     exit(-1);
   }
   double exec_time = omp_get_wtime() - start;
-  fprintf(out_file, "%d,%d,%f\n", threads, sizeof(int) * size, exec_time);
+  fprintf(out_file, "%d,%lld,%f\n", threads, sizeof(int) * size, exec_time);
   return 0;
 }
--- zad2/sc.sh
+++ zad2/sc.sh
@@ -1,10 +1,18 @@
 # max = 32761316000B, for 1000000000 -> terminate called after throwing an instance of 'std::bad_alloc' what():  std::bad_alloc
 # shellcheck disable=SC2034
-for ARR_SIZE in `seq 100 100000 100000000`
+count=0
+total=100
+pstr="[=======================================================================]"
+
+for ARR_SIZE in `seq 100 1000000 100000000`
 do
     # shellcheck disable=SC2043
     for THREADS in 1 2 3 4
     do
-      ./main $ARR_SIZE $THREADS
+      #./main $ARR_SIZE $THREADS
+      sleep 0.5 # this is work
+      count=$(( $count + 1 ))
+      pd=$(( $count * 73 / $total ))
+      printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
     done
 done
