4a25d657ac227e6d5fd9f1e04930b0c9d8a7ae76
bsz <bszlacht@student.agh.edu.pl>
2023-04-18 00:45:56 +0200

    asdsds

--- zad2/schunk.sh
+++ zad2/schunk.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2034
 
-for num_of_buckets in {1000..10000000..1000}
+for num_of_buckets in {100..1000000..100}
 do 
-./bucketsort 10000000 1 $num_of_buckets
+./bucketsort 1000000 1 $num_of_buckets
 done
